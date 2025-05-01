273810cd2a459e3230fbd427c8a8b78c01e14f6b
Dale-Xu1 <dalexu1@outlook.com>
2025-04-30 22:34:17 -0400

    Non-recursive follow set computation

--- test/lalr.go
+++ test/lalr.go
@@ -29,6 +29,7 @@ type LALRParser struct {
     start        NonTerminal
 
     first        map[Symbol]map[Terminal]struct{}
+    follow       map[NonTerminal]map[Terminal]struct{}
 }
 
 func NewLALRParser() *LALRParser {
@@ -46,14 +47,17 @@ func NewLALRParser() *LALRParser {
     return &LALRParser {
         terminals, nonTerminals,
         productions,
-        E,
+        S,
         make(map[Symbol]map[Terminal]struct{}),
+        make(map[NonTerminal]map[Terminal]struct{}),
     }
 }
 
 func (p *LALRParser) Parse() {
     p.findFirst()
+    p.findFollow()
     fmt.Println(p.first)
+    fmt.Println(p.follow)
 }
 
 // Computes the FIRST sets of all symbols in the grammar provided by the parser.
@@ -65,39 +69,85 @@ func (p *LALRParser) findFirst() {
     // Iterative implementation, repeat procedure until no changes are made
     for changed := true; changed; {
         changed = false
-        main: for _, production := range p.productions {
-            left, right := production.Left, production.Right
-            // The FIRST set of a non-terminal is found by going through its productions and taking the union of the FIRST sets
-            // of subsequent symbols until a non-nullable symbol is found (FIRST set does not contain epsilon)
-            for _, s := range right {
-                if s == EPSILON { continue }
-                for f := range p.first[s] {
-                    // Add all elements (ignoring epsilon) in FIRST set of symbol to FIRST set of production LHS non-terminal
-                    if f == EPSILON { continue }
-                    if _, ok := p.first[left][f]; !ok {
-                        p.first[left][f] = struct{}{}
-                        changed = true
-                    }
-                }
-                // All elements in FIRST set have been found if symbol is non-nullable
-                if _, ok := p.first[s][EPSILON]; !ok { continue main }
-            }
-            // If all symbols in the production are nullable, the LHS non-terminal is also nullable
-            if _, ok := p.first[left][EPSILON]; !ok {
-                p.first[left][EPSILON] = struct{}{}
+        for _, production := range p.productions {
+            changed = p.findSequenceFirst(production.Left, production.Right) || changed
+        }
+    }
+}
+
+func (p *LALRParser) findSequenceFirst(left NonTerminal, rule []Symbol) bool {
+    // The FIRST set of a non-terminal is found by going through its productions and taking the union of the FIRST sets
+    // of subsequent symbols until a non-nullable symbol is found (FIRST set does not contain epsilon)
+    changed := false
+    for _, s := range rule {
+        if s == EPSILON { continue }
+        for f := range p.first[s] {
+            // Add all elements (ignoring epsilon) in FIRST set of symbol to FIRST set of production LHS non-terminal
+            if f == EPSILON { continue }
+            if _, ok := p.first[left][f]; !ok {
+                p.first[left][f] = struct{}{}
                 changed = true
+            }
+        }
+        // All elements in FIRST set have been found if symbol is non-nullable
+        if _, ok := p.first[s][EPSILON]; !ok { return changed }
+    }
+    // If all symbols in the production are nullable, the LHS non-terminal is also nullable
+    if _, ok := p.first[left][EPSILON]; !ok {
+        p.first[left][EPSILON] = struct{}{}
+        changed = true
+    }
+    return changed
+}
+
+
+// Computes the FOLLOW sets of all symbols in the grammar provided by the parser.
+func (p *LALRParser) findFollow() {
+    // Initialize empty FOLLOW sets for all non-terminals, start symbol can always be followed by EOF
+    for _, t := range p.nonTerminals { p.follow[t] = make(map[Terminal]struct{}) }
+    p.follow[p.start][END] = struct{}{}
+    // Iterative implementation, repeat procedure until no changes are made
+    for changed := true; changed; {
+        changed = false
+        for _, production := range p.productions {
+            right := production.Right
+            for i, s := range right {
+                // Compute FOLLOW set for each occurrence of a non-terminal based on sequence of symbols that follow it
+                t, ok := s.(NonTerminal); if !ok { continue }
+                changed = p.findSequenceFollow(t, production.Left, right[i + 1:]) || changed
             }
         }
     }
 }
 
-// Computes the FOLLOW sets of all symbols in the grammar provided by the parser.
-func (p *LALRParser) findFollow() {
-
+func (p *LALRParser) findSequenceFollow(t NonTerminal, left NonTerminal, sequence []Symbol) bool {
+    // For a particular non-terminal, look at the FIRST set of the sequence of symbols that follow it
+    // If all symbols are nullable, the FOLLOW set of the LHS is added too
+    changed := false
+    for _, s := range sequence {
+        if s == EPSILON { continue }
+        for f := range p.first[s] {
+            // Add all elements (ignoring epsilon) in FIRST set of symbol to the FOLLOW set
+            if f == EPSILON { continue }
+            if _, ok := p.follow[t][f]; !ok {
+                p.follow[t][f] = struct{}{}
+                changed = true
+            }
+        }
+        // All elements in FOLLOW set have been found if symbol is non-nullable
+        if _, ok := p.first[s][EPSILON]; !ok { return changed }
+    }
+    // If all symbols in the remaining sequence are nullable, the FOLLOW set of the production LHS is added
+    for f := range p.follow[left] {
+        if _, ok := p.follow[t][f]; !ok {
+            p.follow[t][f] = struct{}{}
+            changed = true
+        }
+    }
+    return changed
 }
 
-const EPSILON Terminal = 'ε'
-const END Terminal = '$'
+const (EPSILON Terminal = 'ε'; END = '$')
 const (S NonTerminal = iota; E; T; F)
 var nonTerminalName = map[NonTerminal]string { S: "S", E: "E", T: "T", F: "F" }
 
